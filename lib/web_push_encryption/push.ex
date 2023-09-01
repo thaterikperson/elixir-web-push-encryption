6bb3af149092fe2d9730e1ab09ecfc753a9b3628
Erik Person <erik@thaterikperson.com>
2023-09-01 12:33:41 -0500

    Fix issue with OTP 26.

--- lib/web_push_encryption/push.ex
+++ lib/web_push_encryption/push.ex
@@ -63,8 +63,7 @@ defmodule WebPushEncryption.Push do
       |> Map.put("Crypto-Key", "dh=#{ub64(payload.server_public_key)};" <> headers["Crypto-Key"])
 
     {endpoint, headers} = make_request_params(endpoint, headers, auth_token)
-    options = [ssl: [{:versions, [:"tlsv1.2"]}]]
-    http_client().post(endpoint, payload.ciphertext, headers, options)
+    http_client().post(endpoint, payload.ciphertext, headers, [])
   end
 
   def send_web_push(_message, _subscription, _auth_token, _ttl) do
